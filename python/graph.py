42dc42a4388c442de7132da7e82ec0b71bc95208
Ngari Ndung'u <ngari.ndungu@gmail.com>
2016-10-26 23:19:41 +0300

    Add methods to Vertex class

--- python/graph.py
+++ python/graph.py
@@ -1,5 +1,13 @@
 """ Test implementation of the graph data structure in python
+
 Done following tutorial at https://interactivepython.org/runestone/static/pythonds/Graphs/toctree.html
+
+Methods:
+    addNeighbour
+    getConnections
+    getId
+    getWeight
+    __str__
 """
 
 class Vertex(object):
@@ -15,6 +23,15 @@ class Vertex(object):
         self.neighbours[neighbour] = weight
         return "New neighbour {} added".format(neighbour)
         
-    def getNeighbours(self):
-        return self.neighbours
+    def getConnections(self):
+        return self.neighbours.keys()
+        
+    def getId(self):
+        return self.name 
+        
+    def getWeight(self, neighbour):
+        return self.neighbours[neighbour]
+        
+    def __str__(self):
+        return "{0} connected to {1}".format(self.name, [nbr for nbr in self.neighbours])
     
