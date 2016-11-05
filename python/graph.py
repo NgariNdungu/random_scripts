bc6cede9ebbdbeee5ec0b342723b02e50ee13336
Ngari Ndung'u <ngari.ndungu@gmail.com>
2016-11-05 20:38:20 +0300

    Add graph class

--- python/graph.py
+++ python/graph.py
@@ -2,16 +2,18 @@
 
 Done following tutorial at https://interactivepython.org/runestone/static/pythonds/Graphs/toctree.html
 
-Methods:
-    addNeighbour
-    getConnections
-    getId
-    getWeight
-    __str__
 """
 
 class Vertex(object):
-    """ Create new vertex """
+    """ Create new vertex 
+    
+    Methods:
+        addNeighbour
+        getConnections
+        getId
+        getWeight
+        __str__
+    """
     
     def __init__(self, name):
         """ Create new named Vertex """
@@ -33,5 +35,60 @@ class Vertex(object):
         return self.neighbours[neighbour]
         
     def __str__(self):
-        return "{0} connected to {1}".format(self.name, [nbr for nbr in self.neighbours])
+        return "{0} connected to {1}".format(self.name, [nbr.getId() for nbr in self.neighbours])
     
+class Graph(object):
+    """ Create new Graph 
+    
+    Methods:
+        addVertex
+        getVertex
+        __contains__
+        addEdge
+        getVertices
+        __iter__
+    """
+    
+    def __init__(self):
+        self.vertList = {} # map vertex id to vertex objects
+        self.numVertices = 0
+        
+    def addVertex(self,key):
+        """ create new vertex, add to vertex list and return new vertex """
+        
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        self.numVertices += 1
+        return newVertex
+        
+    def getVertex(self,key):
+        """ check if vertex is in vertList and return it """
+        
+        if key in self.vertList:
+            return self.vertList[key]
+        else:
+            return None
+    
+    def addEdge(self, f, to, weight=0):
+        """ create a new edge
+        
+        add neighbour for existing vertex or create new vertex and add neighbour
+        """
+        
+        if f not in self.vertList:
+            self.addVertex(f)
+        if to not in self.vertList:
+            self.addVertex(to)
+        self.vertList[f].addNeighbour(self.vertList[to], weight)
+        
+    def getVertices(self):
+        """ return a list of all vertices in the graph """
+        
+        return self.vertList.keys()
+        
+    def __contains__(self, key):
+        return key in self.vertList
+    
+    def __iter__(self):
+        return iter(self.vertList.values())
+        
